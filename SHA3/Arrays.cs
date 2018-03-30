105d256787dcb977191dfb115546d0355821d97a
DreTaX <dretax@gmail.hu>
2018-03-30 15:31:08 +0200

    Push

---
+++ SHA3/Arrays.cs
@@ -0,0 +1,22 @@
+﻿namespace SHA3
+{
+    internal abstract class Arrays
+    {
+        public static byte[] Clone(
+            byte[] data)
+        {
+            return data == null ? null : (byte[]) data.Clone();
+        }
+
+        public static void Fill(
+            byte[] buf,
+            byte b)
+        {
+            int i = buf.Length;
+            while (i > 0)
+            {
+                buf[--i] = b;
+            }
+        }
+    }
+}
---
+++ SHA3/SHA3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace SHA3
+{
+    public enum SHA3Bitsize
+    {
+        SHA128 = 128,
+        SHA224 = 224,
+        SHA256 = 256,
+        SHA384 = 384,
+        SHA512 = 512,
+    }
+    
+    public class SHA3 
+    {
+        public static string CalculateSHA3(string filepath, SHA3Bitsize bitsize)
+        {
+            if (!File.Exists(filepath))
+            {
+                throw new FileNotFoundException("Cannot find file: " + filepath);
+            }
+            byte[] bb = File.ReadAllBytes(filepath);
+            Sha3Digest digest = new Sha3Digest((int) bitsize);
+            digest.BlockUpdate(bb, 0, bb.Length);
+            int digestsize = digest.GetDigestSize(); // Calculate the size of our byte array
+            byte[] storage = new byte[digestsize];
+            digest.DoFinal(storage, 0);
+            return BitConverter.ToString(storage, 0);
+        }
+
+        public static byte[] CalculateSHA3Byte(string filepath, SHA3Bitsize bitsize)
+        {
+            if (!File.Exists(filepath))
+            {
+                throw new FileNotFoundException("Cannot find file: " + filepath);
+            }
+            byte[] bb = File.ReadAllBytes(filepath);
+            Sha3Digest digest = new Sha3Digest((int) bitsize);
+            digest.BlockUpdate(bb, 0, bb.Length);
+            int digestsize = digest.GetDigestSize(); // Calculate the size of our byte array
+            byte[] storage = new byte[digestsize];
+            digest.DoFinal(storage, 0);
+            return storage;
+        }
+    }
+}
